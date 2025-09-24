398b5371f368db2fa2486592527dd5d1f36e54c7
ahmed abdelnaby <ahmedabdelnabyfahmy@gmail.com>
2025-09-24 17:32:14 +0300

    feat: enhance compileSlots method in ComponentTagCompiler

--- src/Compiler/ComponentTagCompiler.php
+++ src/Compiler/ComponentTagCompiler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hypervel\Mail\Compiler;
 
+use Hyperf\Stringable\Str;
 use Hyperf\ViewEngine\Blade;
 use Hyperf\ViewEngine\Compiler\ComponentTagCompiler as HyperfComponentTagCompiler;
 use Hyperf\ViewEngine\Contract\FactoryInterface;
@@ -11,6 +12,72 @@ use InvalidArgumentException;
 
 class ComponentTagCompiler extends HyperfComponentTagCompiler
 {
+    /**
+     * Compile the slot tags within the given string.
+     */
+    public function compileSlots(string $value): string
+    {
+        $pattern = "/
+            <
+                \\s*
+                x[\\-\\:]slot
+                (?:\\:(?<inlineName>\\w+(?:-\\w+)*))?
+                (?:\\s+name=(?<name>(\"[^\"]+\"|\\\\'[^\\\\']+\\\\'|[^\\s>]+)))?
+                (?:\\s+\\:name=(?<boundName>(\"[^\"]+\"|\\\\'[^\\\\']+\\\\'|[^\\s>]+)))?
+                (?<attributes>
+                    (?:
+                        \\s+
+                        (?:
+                            (?:
+                                @(?:class)(\\( (?: (?>[^()]+) | (?-1) )* \\))
+                            )
+                            |
+                            (?:
+                                @(?:style)(\\( (?: (?>[^()]+) | (?-1) )* \\))
+                            )
+                            |
+                            (?:
+                                \\{\\{\\s*\\\$attributes(?:[^}]+?)?\\s*\\}\\}
+                            )
+                            |
+                            (?:
+                                [\\w\\-:.@]+
+                                (
+                                    =
+                                    (?:
+                                        \\\"[^\\\"]*\\\"
+                                        |
+                                        \\'[^\\']*\\'
+                                        |
+                                        [^\\'\\\"=<>]+
+                                    )
+                                )?
+                            )
+                        )
+                    )*
+                    \\s*
+                )
+                (?<![\\/=\\-])
+            >
+        /x";
+
+        $value = preg_replace_callback($pattern, function ($matches) {
+            $name = $this->stripQuotes($matches['inlineName'] ?: $matches['name'] ?: $matches['boundName']);
+
+            if (Str::contains($name, '-') && ! empty($matches['inlineName'])) {
+                $name = Str::camel($name);
+            }
+
+            if (! empty($matches['inlineName']) || ! empty($matches['name'])) {
+                $name = "'{$name}'";
+            }
+
+            return " @slot({$name}) ";
+        }, $value);
+
+        return preg_replace('/<\/\s*x[\-\:]slot[^>]*>/', ' @endslot', $value);
+    }
+
     /**
      * Get the component class for a given component alias.
      */
